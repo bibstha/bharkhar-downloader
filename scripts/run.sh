23dd45ab0a4914c64e5a764e37274c4037d44c60
Bibek Shrestha <bibekshrestha@gmail.com>
2014-06-03 13:17:56 +0200

    minor bug fix

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,5 +2,5 @@
 cd /app
 mkdir -p /data/bharkharapp/public
 bundle exec rake download_latest
-cp -r /app/lib/webapp/public /data/bharkharapp/public
+cp -r /app/lib/webapp/public/* /data/bharkharapp/public
 /usr/local/bin/supervisord -c /app/config/supervisord.conf
